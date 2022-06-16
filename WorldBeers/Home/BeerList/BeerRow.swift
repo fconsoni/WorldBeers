c4364597d76a8c691dbf21b64a0687987f10416a
Franco Consoni <franco.consoni@airtime.com>
2022-06-16 19:30:14 -0300

    Started beer row

--- WorldBeers/Home/BeerList/BeerRow.swift
+++ WorldBeers/Home/BeerList/BeerRow.swift
@@ -7,7 +7,54 @@
 
 import Foundation
 import UIKit
+import Kingfisher
 
 final class BeerRow: UICollectionViewCell {
-    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let abvLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let ibuLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private var beer: Beer?
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+        self.imageView.image = nil
+        self.nameLabel.text = ""
+        self.descriptionLabel.text = ""
+        self.abvLabel.text = ""
+        self.ibuLabel.text = ""
+    }
+
+    func show(_ beer: Beer) {
+        self.beer = beer
+
+        self.imageView.kf.setImage(with: URL(string: beer.imageUrl))
+    }
 }
