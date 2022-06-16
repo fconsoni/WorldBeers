670c4a55d4cdc3d1757c55dfdde0e49d266e47cf
Franco Consoni <franco.consoni@airtime.com>
2022-06-16 19:47:39 -0300

    Finished row

--- WorldBeers/Home/BeerList/BeerRow.swift
+++ WorldBeers/Home/BeerList/BeerRow.swift
@@ -14,6 +14,7 @@ final class BeerRow: UICollectionViewCell {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.kf.indicatorType = .activity
+        imageView.image = Images.picturePlaceholder
         return imageView
     }()
 
@@ -43,15 +44,59 @@ final class BeerRow: UICollectionViewCell {
 
     private var beer: Beer?
 
+    init() {
+        super.init(frame: .zero)
+
+        setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func prepareForReuse() {
         super.prepareForReuse()
 
-        self.imageView.image = nil
+        self.imageView.image = Images.picturePlaceholder
         self.nameLabel.text = ""
         self.descriptionLabel.text = ""
-        self.abvLabel.text = ""
-        self.ibuLabel.text = ""
+        self.abvLabel.text = "%"
+        self.ibuLabel.text = "%"
     }
+
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//
+//    }
+
+    private func setupView() {
+        let traitsStackView = UIStackView(arrangedSubviews: [abvLabel, ibuLabel])
+        traitsStackView.translatesAutoresizingMaskIntoConstraints = false
+        traitsStackView.axis = .horizontal
+        traitsStackView.distribution = .equalSpacing
+        traitsStackView.spacing = 8
+
+        [imageView, nameLabel, descriptionLabel, traitsStackView].forEach(self.contentView.addSubview)
+
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+
+            traitsStackView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8),
+            traitsStackView.trailingAnchor.constraint(equalTo: imageView.trailingAnchor),
+
+            nameLabel.topAnchor.constraint(equalTo: traitsStackView.bottomAnchor, constant: 12),
+            nameLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor),
+            nameLabel.trailingAnchor.constraint(equalTo: imageView.trailingAnchor),
+
+            descriptionLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 12),
+            descriptionLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor),
+            descriptionLabel.trailingAnchor.constraint(equalTo: imageView.trailingAnchor),
+            descriptionLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
+        ])
+    }
+
 
     func show(_ beer: Beer) {
         self.beer = beer
@@ -61,7 +106,7 @@ final class BeerRow: UICollectionViewCell {
                                    options: [.cacheOriginalImage])
         self.nameLabel.text = beer.name
         self.descriptionLabel.text = beer.description
-        self.abvLabel.text = "\(beer.abv)%"
-        self.ibuLabel.text = beer.ibu != nil ? "\(beer.ibu?.description ?? "")%" : "N/A"
+        self.abvLabel.text = "ABV: \(beer.abv)%"
+        self.ibuLabel.text = beer.ibu != nil ? "IBU: \(beer.ibu?.description ?? "")%" : "N/A"
     }
 }
